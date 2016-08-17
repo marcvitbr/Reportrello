c1b775fa0b7bd03406baa655fd1f80e0b413300a
Marcelo Vitoria <marcvit@gmail.com>
2016-08-17 13:56:02 -0300

    Create default implementation of IParsingEngine
    The class DefaultArgumentParser was created to be used as the default
    implementation of IParsingEngine, in the CLI. Since the Fluent Command
    Line Parser is being used, the class takes an instance of
    FluentCommandLineParser<ApplicationArguments>, and redirects the call
    to the Parse method to fluent parser's Parse method, returning the
    resulting ApplicationArguments object.

---
+++ Reportrello.CLI/ArgumentsParsing/DefaultArgumentsParser.cs
@@ -0,0 +1,22 @@
+﻿namespace Reportrello.CLI.ArgumentsParsing
+{
+    using System;
+    using Fclp;
+
+    public class DefaultArgumentsParser : IParsingEngine
+    {
+        private readonly FluentCommandLineParser<ApplicationArguments> parser;
+
+        public DefaultArgumentsParser(FluentCommandLineParser<ApplicationArguments> parser)
+        {
+            this.parser = parser;
+        }
+
+        public ApplicationArguments Parse(string[] args)
+        {
+            this.parser.Parse(args);
+
+            return this.parser.Object;
+        }
+    }
+}
