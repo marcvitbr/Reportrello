b2e815ecf47a485f84babdba526346ea6fcc15c0
Marcelo Vitoria <marcvit@gmail.com>
2016-08-17 13:44:58 -0300

    Create a class to hold the command line arguments
    Initially, the fields to be retrieved upon execution are Trello's key
    and access token, the list of ids representing one day, three days and
    five or more days estimates, and the id of the list to be inspected.

---
+++ Reportrello.CLI/ArgumentsParsing/ApplicationArguments.cs
@@ -0,0 +1,15 @@
+﻿namespace Reportrello.CLI.ArgumentsParsing
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ApplicationArguments
+    {
+        public string Key { get; set; }
+        public string Token { get; set; }
+        public List<string> OneDayEstimateIds { get; set; }
+        public List<string> ThreeDaysEstimateIds { get; set; }
+        public List<string> FiveOrMoreDaysEstimateIds { get; set; }
+        public string ListId { get; set; }
+    }
+}
