044a5baf6dd08083f8a504e74b7017a7c330a47a
Marcelo Vitoria <marcvit@gmail.com>
2016-08-17 14:03:17 -0300

    Change Main to use the arguments parser
    An instance of the default parser will be retrieved by using the
    Factory class, and the input arguments will be parsed into an instance
    of ApplicationArguments. The data will then be used to generate the
    report.

--- Reportrello.CLI/Program.cs
+++ Reportrello.CLI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Reportrello.CLI.ArgumentsParsing;
 using Reportrello.Kanban;
 using Reportrello.Report;
 using Reportrello.Trello;
@@ -8,33 +9,22 @@ namespace Reportrello.CLI
 {
     class MainClass
     {
-        private const string Key = "YOUR_KEY";
-
-        private const string Token = "YOUR_TOKEN";
-
-        private static readonly IEnumerable<string> OneDayEstimateIds =
-            new[] { "57290990b0dfecc6d1276aab", "57279be0b0dfecc6d122a011" };
-
-        private static readonly IEnumerable<string> ThreeDaysEstimateIds =
-            new[] { "57290990b0dfecc6d1276aac", "57279be0b0dfecc6d122a012" };
-
-        private static readonly IEnumerable<string> FiveOrMoreDaysEstimateIds =
-            new[] { "57290990b0dfecc6d1276aad", "57279be0b0dfecc6d122a013" };
-        
         public static void Main(string[] args)
         {
-            var account = new TrelloAccount(Key, Token);
+            var parser = ParsingEngineFactory.CreateDefaultArgumentsParser();
 
-            var listTypesIds = new ListTypesIds(args[0]);
+            var arguments = parser.Parse(args);
+
+            var account = new TrelloAccount(arguments.Key, arguments.Token);
 
             var estimateIds = new EstimateIds(
-                OneDayEstimateIds,
-                ThreeDaysEstimateIds,
-                FiveOrMoreDaysEstimateIds);
+                arguments.OneDayEstimateIds,
+                arguments.ThreeDaysEstimateIds,
+                arguments.FiveOrMoreDaysEstimateIds);
 
             var reporter = new Reporter(account, estimateIds);
 
-            var cards = reporter.AllCardsInListAsync(listTypesIds.DoneListId).Result;
+            var cards = reporter.AllCardsInListAsync(arguments.ListId).Result;
 
             foreach (var card in cards)
             {
