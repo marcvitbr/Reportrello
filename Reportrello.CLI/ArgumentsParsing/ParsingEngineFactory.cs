b28c594b677c45d5812953a53850092875fcfa5d
Marcelo Vitoria <marcvit@gmail.com>
2016-08-17 13:59:55 -0300

    Create Factory class for parsing engines
    The class only one method called CreateDefaultArgumentsParser, that
    will create and configure an instance of the FluentCommandLineParser
    class.

---
+++ Reportrello.CLI/ArgumentsParsing/ParsingEngineFactory.cs
@@ -0,0 +1,42 @@
+﻿namespace Reportrello.CLI.ArgumentsParsing
+{
+    using System;
+    using System.Collections.Generic;
+    using Fclp;
+
+    public static class ParsingEngineFactory
+    {
+        public static IParsingEngine CreateDefaultArgumentsParser()
+        {
+            var fluentParser = new FluentCommandLineParser<ApplicationArguments>();
+
+            fluentParser.Setup(arg => arg.Key)
+                        .As('k', "key")
+                        .Required();
+
+            fluentParser.Setup(arg => arg.Token)
+                        .As('a', "token")
+                        .Required();
+
+            fluentParser.Setup(arg => arg.OneDayEstimateIds)
+                        .As('o', "one-day-ids")
+                        .Required();
+
+            fluentParser.Setup(arg => arg.ThreeDaysEstimateIds)
+                        .As('t', "three-days-ids")
+                        .Required();
+
+            fluentParser.Setup(arg => arg.FiveOrMoreDaysEstimateIds)
+                        .As('f', "five-days-ids")
+                        .Required();
+
+            fluentParser.Setup(arg => arg.ListId)
+                        .As('l', "list")
+                        .Required();
+
+            var defaultParser = new DefaultArgumentsParser(fluentParser);
+
+            return defaultParser;
+        }
+    }
+}
