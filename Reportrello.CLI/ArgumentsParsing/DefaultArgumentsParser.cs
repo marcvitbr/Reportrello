c2bdd718c40f3a9a129096565c2049555e0cb418
Marcelo Vitoria <marcvit@gmail.com>
2016-09-19 15:51:39 -0300

    Modify DefaultArgumentsParser to consider board id argument
    The parser is now throwing an exception in case the user passed both
    the id of the list and the id of the board. As of now, the program will
    only work with one at a time.

--- Reportrello.CLI/ArgumentsParsing/DefaultArgumentsParser.cs
+++ Reportrello.CLI/ArgumentsParsing/DefaultArgumentsParser.cs
@@ -16,6 +16,16 @@
         {
             this.parser.Parse(args);
 
+            var arguments = this.parser.Object;
+
+            var hasBoardAndList = !string.IsNullOrWhiteSpace(arguments.BoardId)
+                               && !string.IsNullOrWhiteSpace(arguments.ListId);
+
+            if(hasBoardAndList)
+            {
+                throw new ArgumentException("You can specify the board id OR list id, but not both.");
+            }
+
             return this.parser.Object;
         }
     }
