472c86d41a76c156d70ec78455fc385342f198ce
Marcelo Vitoria <marcvit@gmail.com>
2016-09-19 15:49:20 -0300

    Create interface to represent any Report
    The IReport interface was created, with one method called Generate,
    which gets an instance of ApplicationArguments.

---
+++ Reportrello.CLI/Reports/IReport.cs
@@ -0,0 +1,10 @@
+﻿namespace Reportrello.CLI.Reports
+{
+    using System;
+    using Reportrello.CLI.ArgumentsParsing;
+
+    public interface IReport
+    {
+        void Generate(ApplicationArguments arguments);
+    }
+}
