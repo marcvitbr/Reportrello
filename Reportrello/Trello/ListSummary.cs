bc94f212f88d8106e64424da4436e67f9a6994d3
Marcelo Vitoria <marcvit@gmail.com>
2016-09-19 15:45:37 -0300

    Create model to represent summary of a List in Trello

---
+++ Reportrello/Trello/ListSummary.cs
@@ -0,0 +1,32 @@
+﻿namespace Reportrello.Trello
+{
+    using System;
+
+    public class ListSummary
+    {
+        public ListSummary(string id, string name, int cardsCount)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.CardsCount = cardsCount;
+        }
+
+        public string Id
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public int CardsCount
+        {
+            get;
+            set;
+        }
+    }
+}
