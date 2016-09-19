26c731d4d7aea7ae650357f094e4271cdd7149e6
Marcelo Vitoria <marcvit@gmail.com>
2016-09-19 15:45:01 -0300

    Create model to represent a List in Trello

---
+++ Reportrello/Trello/List.cs
@@ -0,0 +1,23 @@
+﻿namespace Reportrello.Trello
+{
+    using System;
+
+    public class List
+    {
+        public List()
+        {
+        }
+
+        public string Id
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
